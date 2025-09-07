8dadcb4f1e50133588d940fcbc1bc05684bed365
SHAHANAS-sana <149222540+SHAHANAS-sana@users.noreply.github.com>
2025-09-08 00:03:46 +0530

    Add files via upload

--- BuildConfig.java
+++ BuildConfig.java
@@ -1,10 +1,10 @@
 /**
  * Automatically generated file. DO NOT MODIFY
  */
-package io.flutter.plugins.urllauncher;
+package io.flutter.plugins.sharedpreferences;
 
 public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
-  public static final String LIBRARY_PACKAGE_NAME = "io.flutter.plugins.urllauncher";
+  public static final String LIBRARY_PACKAGE_NAME = "io.flutter.plugins.sharedpreferences";
   public static final String BUILD_TYPE = "debug";
 }
