7b5edd0e389326f5a3d766ee1284b38fd1b44893
SHAHANAS-sana <149222540+SHAHANAS-sana@users.noreply.github.com>
2025-09-08 21:15:05 +0530

    Add files via upload

--- BuildConfig.java
+++ BuildConfig.java
@@ -1,10 +1,10 @@
 /**
  * Automatically generated file. DO NOT MODIFY
  */
-package io.flutter.plugins.sharedpreferences;
+package com.razorpay.razorpay_flutter;
 
 public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
-  public static final String LIBRARY_PACKAGE_NAME = "io.flutter.plugins.sharedpreferences";
+  public static final String LIBRARY_PACKAGE_NAME = "com.razorpay.razorpay_flutter";
   public static final String BUILD_TYPE = "debug";
 }
